49d9aea253adf7dc2c4efc27f76557dae0d612d1
Diego Balseiro <dbalseiro@stackbuiilders.com>
2024-04-05 14:35:28 -0500

    Pacjage binaries

--- cnb-components/buildpacks/juvo-poetry-buildpack/build.sh
+++ cnb-components/buildpacks/juvo-poetry-buildpack/build.sh
@@ -7,3 +7,5 @@ for BIN in detect build; do
   echo Building $BIN
   go build $@ -ldflags="-s -w" -o ./bin/$BIN ./cmd/$BIN/main.go
 done
+
+tar czf package/juvo-poetry-buildpack.tar.gz bin buildpack.toml
